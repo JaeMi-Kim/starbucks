48aa88e04918b13177440c28476d63e2b0b11b94
JaeMi-Kim <true0208@naver.com>
2023-07-19 14:49:16 +0900

    로그인 페이지 완성

--- js/main.js
+++ js/main.js
@@ -140,6 +140,5 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 2023
+
 
